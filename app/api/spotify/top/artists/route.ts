233ba7ec61d5ed8a4fbcfa730af75130acb18668
JB Pryor <jpryor35@icloud.com>
2026-01-18 11:53:40 -0800

    working on ui/button effects

--- app/api/spotify/top/artists/route.ts
+++ app/api/spotify/top/artists/route.ts
@@ -3,7 +3,7 @@ import { spotifyFetch } from "@/lib/spotify"
 
 export async function GET() {
   const data = await spotifyFetch(
-    "/me/top/artists?limit=50&time_range=long_term"
+    "/me/top/artists?limit=50&time_range=medium_term"
   )
 
   return NextResponse.json(data.items)
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,7 @@ type User = {
   profileImage: string | null
 }
 
-type Track = {
+type Song = {
   name: string
   artist: string
   albumImage?: string
@@ -25,11 +25,16 @@ export default function Home() {
 
   const [user, setUser] = useState<User | null>(null)
 
-  const [topTracks, setTopTracks] = useState<Track[]>([])
+  const [topSongs, setTopTracks] = useState<Song[]>([])
   const [topArtists, setTopArtists] = useState<Artist[]>([])
   const [topGenres, setTopGenres] = useState<string[]>([])
-  const [lastPlayed, setLastPlayed] = useState<Track | null>(null)
-  const [recommendation, setRecommendation] = useState<Track | null>(null)
+  const [lastPlayed, setLastPlayed] = useState<Song | null>(null)
+  const [recommendation, setRecommendation] = useState<Song | null>(null)
+
+  const [showArtists, setShowArtists] = useState(false);
+  const [showGenres, setShowGenres] = useState(false);
+  const [showSongs, setShowSongs] = useState(false);
+
 
   useEffect(() => {
     fetch("/api/spotify/me")
@@ -91,34 +96,31 @@ export default function Home() {
           </div>
         </div>
         
-        <h1 className="text-6xl text-center pt-10">Welcome {user.username}</h1>
-        <h2 className="text-4xl text-center p-2">to Your Spotify Dashboard</h2>
+        <h1 className="text-6xl text-center pt-10 font-bold">Welcome {user.username}</h1>
+        <h2 className="text-4xl text-center p-2 pb-10">to Your Spotify Dashboard</h2>
 
         <div className="flex flex-col gap-2 justify-center items-center text-xl">
           <div className="flex gap-2">
-            <div className="bg-neutral-800 p-5 rounded-lg">{user.country}</div>
-            <div className="bg-neutral-800 rounded-lg"><button className="cursor-pointer p-5"><h2>View Top Artists</h2>{topArtists.map((artist) => (
-              <div key={artist.name}>{artist.name}</div>
-              ))}</button></div>
+            <div className="bg-neutral-800 p-5 rounded-lg"><span className="pr-5 text-2xl">🇺🇸</span>{user.country}</div>
+
+
+            <div className="bg-neutral-800 rounded-lg hover:bg-neutral-700 transition ease-linear"><button onClick={() => setShowArtists(true)} className="cursor-pointer p-5"><h2>View Top Artists</h2>
+            </button></div>
+
+            <div className="bg-neutral-800 rounded-lg hover:bg-neutral-700 transition ease-linear">
+                <button onClick={() => setShowSongs(true)} className="p-5 cursor-pointer"><div className="rounded-lg p-5"><h2>View Top Songs</h2></div></button>
+            </div>
+            <div className="bg-neutral-800 rounded-lg hover:bg-neutral-700 transition ease-linear">
+                <button onClick={() => setShowGenres(true)} className="p-5 cursor-pointer"><div className="rounded-lg p-5"><h2>View Top Genres</h2></div></button>
+            </div>
           </div>
 
-          <div className="flex gap-2">
-            <div className="bg-neutral-800 rounded-lg">
-              <button className="p-5 cursor-pointer"><h2>View Top Songs</h2>{topTracks.map((track) => (
-                <div key={track.name}>
-                  {track.name} • {track.artist}
-                </div>
-                ))}
-              </button></div>
-            <div className="bg-neutral-800 rounded-lg"><button className="p-5 cursor-pointer"><h2>View Top Genres</h2>{topGenres.join(", ")}</button></div>
-          </div>
-
-          <div className="bg-neutral-800 p-5 rounded-lg">
+          {/*<div className="bg-neutral-800 p-5 rounded-lg">
           🎉 🥳 According to your recent song history you are a partyer loving to dance and have a good time! 🎉🥳
-          </div>
+          </div>*/}
 
           <div className="flex gap-2">
-            {recommendation && (<div className="bg-neutral-800 p-5 rounded-lg"><h2>Next Song Recommendation</h2>{recommendation.name} • {recommendation.artist}</div>)}
+            {recommendation && (<div className="bg-neutral-800 p-5 rounded-lg"><h2>Next Song Recommendation:</h2>{recommendation.name} • {recommendation.artist}</div>)}
             {lastPlayed && ( <div className="bg-neutral-800 p-5 rounded-lg"><h2>Last Song Listened To:</h2>{lastPlayed.name} • {lastPlayed.artist}</div> )}
           </div>
 
@@ -127,6 +129,82 @@ export default function Home() {
         </div>
 
       </div>
+
+
+      {showArtists && (
+        <div className="fixed inset-0 bg-black/70 flex justify-center items-center z-50 transition ease-linear">
+          
+          <div className="bg-neutral-900 p-6 rounded-lg text-center w-80">
+            <h2 className="text-2xl mb-4">Top Artists</h2>
+
+            {topArtists.slice(0, 5).map((artist) => (
+              <div key={artist.name} className="py-1">
+                {artist.name}
+              </div>
+            ))}
+
+            <button
+              onClick={() => setShowArtists(false)}
+              className="mt-4 px-4 py-2 bg-neutral-700 rounded"
+            >
+              Close
+            </button>
+          </div>
+
+        </div>
+      )}
+
+      {showGenres && (
+         <div className="fixed inset-0 bg-black/70 flex justify-center items-center z-50 transition ease-linear">
+          
+         <div className="bg-neutral-900 p-6 rounded-lg text-center w-80">
+           <h2 className="text-2xl mb-4">Top Genres</h2>
+
+           {topGenres.slice(0, 5).map((genre) => (
+             <div key={genre} className="py-1">
+               {genre}
+             </div>
+           ))}
+
+           <button
+             onClick={() => setShowGenres(false)}
+             className="mt-4 px-4 py-2 bg-neutral-700 rounded"
+           >
+             Close
+           </button>
+         </div>
+
+       </div>
+      )}
+
+
+  {showSongs && (
+         <div className="fixed inset-0 bg-black/70 flex justify-center items-center z-50 transition ease-linear">
+          
+         <div className="bg-neutral-900 p-6 rounded-lg text-center w-80">
+           <h2 className="text-2xl mb-4">Top Songs</h2>
+
+           {topSongs.slice(0, 5).map((song) => (
+             <div key={song.name} className="py-1">
+               {song.name}
+               {"     "}
+               •
+               {"     "}
+               {song.artist}
+             </div>
+           ))}
+
+           <button
+             onClick={() => setShowSongs(false)}
+             className="mt-4 px-4 py-2 bg-neutral-700 rounded"
+           >
+             Close
+           </button>
+         </div>
+
+       </div>
+      )}
+
     </>
   );
 }
